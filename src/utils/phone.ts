1ba5da22c7352858307c6316d447b2bd4d6b2486
beom220 <beom220@gmail.com>
2023-03-13 00:20:55 +0900

    feat: 핸드폰 번호 유효성 검사

--- src/utils/phone.ts
+++ src/utils/phone.ts
@@ -1,6 +1,7 @@
 const isPhoneNumberValid = (phoneNumber: string): boolean => {
+  const phoneNumberWithoutHyphen = phoneNumber.replace(/-/g, '');
   const regex = /^(010)(\d{8})$|^01([1|6|7|8|9])(\d{7,8})$/;
-  return regex.test(phoneNumber);
+  return regex.test(phoneNumberWithoutHyphen);
 };
 
 export default isPhoneNumberValid;
