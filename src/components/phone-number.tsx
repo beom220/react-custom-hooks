347698837818024efe86ab27fcd401c1a7dbe0b6
beom220 <beom220@gmail.com>
2023-03-13 00:18:05 +0900

    feat: 핸드폰 번호 유효성 검사

--- src/components/phone-number.tsx
+++ src/components/phone-number.tsx
@@ -1,12 +1,19 @@
 import usePhoneNumber from '@/hooks/usePhoneNumber';
+import isPhoneNumberValid from '@/utils/phone';
 
 const PhoneNumber = () => {
-  const { phoneNumber, onChangePhoneNumber } = usePhoneNumber('01012341234');
+  const { phoneNumber, onChangePhoneNumber } = usePhoneNumber('');
+
   return (
     <>
       <h1>핸드폰 번호</h1>
       <div>
         <input type="text" value={phoneNumber} onChange={onChangePhoneNumber} />
+        <input
+          type="button"
+          value="검사"
+          onClick={() => alert(isPhoneNumberValid(phoneNumber))}
+        />
       </div>
     </>
   );
---
+++ src/utils/phone.ts
@@ -0,0 +1,6 @@
+const isPhoneNumberValid = (phoneNumber: string): boolean => {
+  const regex = /^(010)(\d{8})$|^01([1|6|7|8|9])(\d{7,8})$/;
+  return regex.test(phoneNumber);
+};
+
+export default isPhoneNumberValid;
