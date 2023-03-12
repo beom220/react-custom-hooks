eee0cf4b00fa122c0628f02e492a1a935f058f75
beom220 <beom220@gmail.com>
2023-03-13 00:17:44 +0900

    modify: 타입을 이용하여 가독성 개선

--- src/hooks/usePhoneNumber.ts
+++ src/hooks/usePhoneNumber.ts
@@ -1,55 +1,55 @@
 import { useState, ChangeEvent, useCallback } from 'react';
 
+type FormatPhoneNumberFunction = (input: string) => string;
+type SetFormattedPhoneNumberFunction = (formattedPhoneNumber: string) => void;
+type OnChangePhoneNumberFunction = (
+  event: ChangeEvent<HTMLInputElement>,
+) => void;
 type UsePhoneNumberReturnType = {
   phoneNumber: string;
-  onChangePhoneNumber: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChangePhoneNumber: OnChangePhoneNumberFunction;
 };
 
 const usePhoneNumber = (
   initialPhoneNumber: string,
 ): UsePhoneNumberReturnType => {
-  const formatPhoneNumber = useCallback((input: string): string => {
+  const [phoneNumber, setPhoneNumber] = useState(initialPhoneNumber);
+
+  const formatPhoneNumber: FormatPhoneNumberFunction = useCallback(input => {
     const phoneNumberWithoutHyphen = input.replace(/-/g, '');
     const phoneNumberLength = phoneNumberWithoutHyphen.length;
+    const firstThreeDigits = phoneNumberWithoutHyphen.slice(0, 3);
 
     if (phoneNumberLength < 4) {
       return phoneNumberWithoutHyphen;
     }
     if (phoneNumberLength < 7) {
-      return `${phoneNumberWithoutHyphen.slice(
-        0,
-        3,
-      )}-${phoneNumberWithoutHyphen.slice(3)}`;
+      return `${firstThreeDigits}-${phoneNumberWithoutHyphen.slice(3)}`;
     }
     if (phoneNumberLength < 11) {
-      return `${phoneNumberWithoutHyphen.slice(
-        0,
-        3,
-      )}-${phoneNumberWithoutHyphen.slice(
-        3,
-        6,
-      )}-${phoneNumberWithoutHyphen.slice(6)}`;
+      const middleDigits = phoneNumberWithoutHyphen.slice(3, 6);
+      const lastDigits = phoneNumberWithoutHyphen.slice(6);
+      return `${firstThreeDigits}-${middleDigits}-${lastDigits}`;
     }
-    return `${phoneNumberWithoutHyphen.slice(
-      0,
-      3,
-    )}-${phoneNumberWithoutHyphen.slice(3, 7)}-${phoneNumberWithoutHyphen.slice(
-      7,
-      11,
-    )}`;
+    const secondGroupDigits = phoneNumberWithoutHyphen.slice(3, 7);
+    const thirdGroupDigits = phoneNumberWithoutHyphen.slice(7, 11);
+    return `${firstThreeDigits}-${secondGroupDigits}-${thirdGroupDigits}`;
   }, []);
 
-  const [phoneNumber, setPhoneNumber] = useState<string>(() =>
-    formatPhoneNumber(initialPhoneNumber),
-  );
-
-  const onChangePhoneNumber = useCallback(
-    (event: ChangeEvent<HTMLInputElement>): void => {
-      const input = event.target.value;
-      const formattedPhoneNumber = formatPhoneNumber(input);
+  const setFormattedPhoneNumber: SetFormattedPhoneNumberFunction = useCallback(
+    formattedPhoneNumber => {
       setPhoneNumber(formattedPhoneNumber);
     },
-    [formatPhoneNumber],
+    [],
+  );
+
+  const onChangePhoneNumber: OnChangePhoneNumberFunction = useCallback(
+    event => {
+      const input = event.target.value;
+      const formattedPhoneNumber = formatPhoneNumber(input);
+      setFormattedPhoneNumber(formattedPhoneNumber);
+    },
+    [formatPhoneNumber, setFormattedPhoneNumber],
   );
 
   return { phoneNumber, onChangePhoneNumber };
